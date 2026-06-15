5d17249b191ec8548a3c0fe56106902d05583103
Ryan <ryan@example.com>
2026-06-15 17:46:03 -0300

    Configure Firebase project

--- firebase-config.js
+++ firebase-config.js
@@ -1,8 +1,8 @@
 window.firebaseConfig = {
-  apiKey: "COLE_AQUI",
-  authDomain: "COLE_AQUI",
-  projectId: "COLE_AQUI",
-  storageBucket: "COLE_AQUI",
-  messagingSenderId: "COLE_AQUI",
-  appId: "COLE_AQUI"
+  apiKey: "AIzaSyBKXag3Gf3MM-0uLAU7CixAdlhJurY1BUQ",
+  authDomain: "rr-reparacao-manager.firebaseapp.com",
+  projectId: "rr-reparacao-manager",
+  storageBucket: "rr-reparacao-manager.firebasestorage.app",
+  messagingSenderId: "802685910397",
+  appId: "1:802685910397:web:a40ac971bd941a3c40a014"
 };
